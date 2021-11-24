980def3262f8e6419230d59c4e9d6c68f432581b
ErnestsLavrinovicsalias] <ernests.lavrinovics@gmail.com>
2021-11-24 14:18:49 +0100

    add toRGB function

--- src/functions.py
+++ src/functions.py
@@ -1,14 +1,22 @@
 import time
 import numpy as np
+import matplotlib as plt
+import torch
+from skimage.color import lab2rgb, rgb2lab, rgb2gray
 
-def is_rgb(img):
-    return len(img.shape) == 3 and img.shape[2] == 3
-
-def to_rgb(img):
-    if is_rgb(img):
-        return img
-    else:
-        return np.dstack((img,) * 3)
+def to_rgb(grayscale_input, ab_input, save_path=None, save_name=None):
+  '''Show/save rgb image from grayscale and ab channels
+     Input save_path in the form {'grayscale': '/path/', 'colorized': '/path/'}'''
+  plt.clf() # clear matplotlib 
+  color_image = torch.cat((grayscale_input, ab_input), 0).numpy() # combine channels
+  color_image = color_image.transpose((1, 2, 0))  # rescale for matplotlib
+  color_image[:, :, 0:1] = color_image[:, :, 0:1] * 100
+  color_image[:, :, 1:3] = color_image[:, :, 1:3] * 255 - 128   
+  color_image = lab2rgb(color_image.astype(np.float64))
+  grayscale_input = grayscale_input.squeeze().numpy()
+  if save_path is not None and save_name is not None: 
+    plt.imsave(arr=grayscale_input, fname='{}{}'.format(save_path['grayscale'], save_name), cmap='gray')
+    plt.imsave(arr=color_image, fname='{}{}'.format(save_path['colorized'], save_name))
 
 class AverageMeter(object):
   '''A handy class from the PyTorch ImageNet tutorial''' 
