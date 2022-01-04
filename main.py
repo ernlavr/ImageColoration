6b0975e85a57d4dcd792390fe35286c8f392cf4d
ErnestsLavrinovicsalias] <ernests.lavrinovics@gmail.com>
2022-01-04 15:56:45 +0100

    Add split dataset, add commandline arg parsing in main

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import torch.utils.data
 import torch.nn as nn
 from torchvision import transforms
 import argparse
+import os
 
 import src.Dataset as ds
 import src.network
@@ -14,8 +15,56 @@ def parseArgs():
   parser = argparse.ArgumentParser()
 
   parser.add_argument('-e', type=int, help="Number of epochs")
+  parser.add_argument('--ts', type=str, help="Specify relative path to a training set folder")
+  parser.add_argument('--vs', type=str, help="Specify relative path to a validation set folder")
+  parser.add_argument('--pretrained', type=str, help="Indicates to use a pre-trained weight")
   parser.add_argument('-b', action='store_true', help="Automatically break after first batch.. Used for debugging CPU output")
+  parser.add_argument('--skip_training', action='store_true', help="Proceed straight to validation")
   return parser.parse_args()
+
+def getDatasets(args):
+  """ Returns training/testing dataset. Either it splits the data contained in dummyData/ColorfulOriginal
+      or uses pre-specified folders depending
+  """
+
+  # Initialize the dataset
+  trainingTransforms = transforms.Compose([transforms.ToTensor()])
+  dataset = ds.ImageDataset("dummyData/ColorfulOriginal", transform=trainingTransforms)
+
+  # Calculate 90%-10% for a split between train-test set
+  dsLen = len(dataset)
+  trainLen = int(dsLen * 0.9)
+  valLen = dsLen - trainLen
+
+  # Split training and testing sets and create DataLoader object
+  trainSet, valSet = torch.utils.data.random_split(dataset, [trainLen, valLen])
+  trainSet = torch.utils.data.DataLoader(trainSet, batch_size=10, shuffle=True)
+  valSet = torch.utils.data.DataLoader(valSet, batch_size=10, shuffle=True)
+
+  # Overwrite the dummy-data with pre-specified datasets if applicable. Inefficient but simple..
+  if(os.path.exists(args.ts)):
+    set = ds.ImageDataset(args.ts, transform=trainingTransforms)
+    trainSet = torch.utils.data.DataLoader(set, batch_size=10, shuffle=True)
+
+  if(os.path.exists(args.vs)):
+    set = ds.ImageDataset(args.vs, transform=trainingTransforms)
+    valSet = torch.utils.data.DataLoader(set, batch_size=10, shuffle=True)
+
+  return trainSet, valSet
+
+
+def getModel(args):
+  """Loads a model with either pretrained weights if framework is started with "--pretrained" flag;
+     or blank model if without
+  """
+  model = src.network.eccv16()
+  if(os.path.exists(args.pretrained)):
+    model.load_state_dict(torch.load(args.pretrained))
+    print(f"Loaded pretrained weights from path {args.pretrained}")
+  
+  return model
+
+    
 
 def main():
   # Get args
@@ -25,23 +74,11 @@ def main():
   device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
   print(f"Torch set to compute on device: {device}")
 
-
-  # Specify transforms and initialize the dataset
-  trainingTransforms = transforms.Compose([transforms.ToTensor()])
-  dataset = ds.ImageDataset("dummyData/ColorfulOriginal", transform=trainingTransforms)
-
-  # Calculate 90%-10% for a split between train-test set
-  dsLen = len(dataset)
-  trainLen = int(dsLen * 0.9)
-  testLen = dsLen - trainLen
-
-  # Split training and testing sets and create DataLoader object
-  trainSet, testSet = torch.utils.data.random_split(dataset, [trainLen, testLen])
-  trainset = torch.utils.data.DataLoader(trainSet, batch_size=10, shuffle=True)
-  testingData = torch.utils.data.DataLoader(testSet, batch_size=10, shuffle=True)
-
+  # Get datasets
+  trainSet, validationSet = getDatasets(args)
+  
   # Initialize Neural Net object and push it to CUDA if available
-  model = src.network.eccv16()
+  model = getModel(args)
   if(device.type != 'cpu'):
     print(f"Pushing model to CUDA")
     model.cuda()
@@ -59,15 +96,17 @@ def main():
   # Train model
   for epoch in range(epochs):
     # Train for one epoch, then validate
-    train(trainset, model, criterion, optimizer, epoch, device, args.b)
+    if(args.skip_training is None):
+      train(trainSet, model, criterion, optimizer, epoch, device, args.b)
     with torch.no_grad():
-      losses = validate(testingData, model, criterion, save_images, epoch, device)
+      losses = validate(validationSet, model, criterion, save_images, epoch, device)
 
     # Save checkpoint and replace old best model if current model is better
     if losses < best_losses:
       best_losses = losses
       pathlib.Path('checkpoints').mkdir(parents=True, exist_ok=True) 
-      torch.save(model.state_dict(), 'checkpoints/model-epoch-{}-losses-{:.3f}.pth'.format(epoch+1,losses))
+      torch.save(model.state_dict(), 
+                'checkpoints/model-epoch-{}-losses-{:.3f}.pth'.format(epoch+1,losses))
 
 if __name__ == '__main__':
   main()
